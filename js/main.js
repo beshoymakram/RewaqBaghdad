f76f6b3b630ab94f3960d94c200cba2492e75aa5
Beshoy Makram <83455686+beshoymakram@users.noreply.github.com>
2023-02-02 14:34:23 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,102 @@
+$(document).ready(function(){
+    $("#owl-1").owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        dots:false,
+        autoplay:true,
+        autoplayTimeout:4000,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
+});
+
+$(document).ready(function(){
+    $("#owl-2").owlCarousel({
+        loop:false,
+        margin:10,
+        nav:false,
+        dots:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+});
+
+$(document).ready(function(){
+    $("#owl-3").owlCarousel({
+        loop:false,
+        margin:10,
+        nav:false,
+        dots:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+});
+
+$(document).ready(function(){
+    $("#owl-4").owlCarousel({
+        loop:false,
+        margin:10,
+        nav:true,
+        dots:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+});
+
+$(document).ready(function(){
+    $("#owl-5").owlCarousel({
+        loop:false,
+        margin:10,
+        nav:true,
+        dots:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
+});
